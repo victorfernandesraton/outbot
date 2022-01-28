081a500fb165c68fa9f01585052e2bb792fb1e24
victorfernandesraton <vfbraton@gmail.com>
2022-01-28 09:12:30 -0300

    basic load

--- src/config/geminin.go
+++ src/config/geminin.go
@@ -1,6 +1,14 @@
 package config
 
+import "os"
+
 type GemininConfig struct {
 	Key    string
 	Domain string
+}
+
+func (config *GemininConfig) New() *GemininConfig {
+	config.Key = os.Getenv("GEMININ_KEY")
+	config.Key = os.Getenv("GEMININ_DOMAIN")
+	return config
 }
