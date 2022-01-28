9cbae255d5ac630a9eaf6593033ca7b7897d8936
victorfernandesraton <vfbraton@gmail.com>
2022-01-28 01:48:25 -0300

    ignore

--- src/entity/price.go
+++ src/entity/price.go
@@ -2,10 +2,42 @@ package entity
 
 import (
 	"database/sql"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
 )
 
 type Price struct {
 	Crypto     Crypto
 	Evaluation float64
 	Date       sql.NullTime
+}
+
+func (p *Price) MarshalJSON() ([]byte, error) {
+	var dataStr interface{}
+	if p.Crypto.CreatedAt.Valid == true {
+		dataStr = struct {
+			Crypto     Crypto    `json:"crypto"`
+			Evaluation float64   `json:"evaluation"`
+			Date       time.Time `json:"date"`
+		}{
+			Crypto:     p.Crypto,
+			Evaluation: p.Evaluation,
+			Date:       p.Date.Time,
+		}
+	} else {
+		dataStr = struct {
+			Crypto     Crypto  `json:"crypto"`
+			Evaluation float64 `json:"evaluation"`
+		}{
+			Crypto:     p.Crypto,
+			Evaluation: p.Evaluation,
+		}
+	}
+	data, err := json.Marshal(dataStr)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("JSON Invalid parse %v", err.Error()))
+	}
+	return data, nil
 }
---
+++ src/entity/price_test.go
@@ -0,0 +1,35 @@
+package entity_test
+
+import (
+	"database/sql"
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.comvictorfernandesraton/outbot/src/entity"
+)
+
+func TestValidParser(t *testing.T) {
+	crypto := &entity.Crypto{
+		Name:   "Etherium",
+		Symbol: "ETH",
+		CreatedAt: sql.NullTime{
+			Time:  time.Date(2019, 4, 21, 0, 0, 0, 0, time.UTC),
+			Valid: true,
+		},
+		Site: "www.eth.com",
+	}
+	data := &entity.Price{
+		Crypto:     *crypto,
+		Evaluation: 20,
+		Date: sql.NullTime{
+			Time:  time.Date(2020, 4, 10, 0, 0, 0, 0, time.UTC),
+			Valid: true,
+		},
+	}
+	_, err := json.Marshal(data)
+	if err != nil {
+		t.Error("Invalid parse")
+	}
+
+}
